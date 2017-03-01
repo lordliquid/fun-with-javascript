119304af51ff492e6925342ece2ede896653aa0f
Robert Cutright <rdcpcguy@gmail.com>
2017-02-28 23:32:24 -0600

    Update state.js

--- state.js
+++ state.js
@@ -1,36 +1,67 @@
-class State {
-  constructor(name) {
-    this.name = name;
-    this.count = 0;
-  }
-  
-  setState(obj) {
+'use strict';
+let map = new WeakMap();
 
-    if(typeof obj !== 'object'){
-      console.log('This was called');
-      return this;
-    }
-
-    for(var o in obj){
-      this.count++;
-      let key = o;
-      this[key] = obj[key];
-    }
-    
-    return this;
+let internal = function (object) {
+  if (!map.has(object)) {
+    map.set(object, {});
   }
-
-  
-  getState() {
-    return this;
-  }
-  
+  return map.get(object);
 };
 
-var state = new State('My State');
-var myfunc = () => { return 'function' }
-state.setState({test: myfunc });
+class Store {
+  constructor() {
+    let hist = [];
+    let state = {};
+    this.count = 0;
+    internal(this).hist = hist;
+    internal(this).state = state;
+  }
+};
+  
+Store.prototype.setState = function (obj) {
+  if(typeof obj !== 'object'){
+    console.error('.setState() expects an object and instead received ' + typeof obj + ' type.');
+    return this;
+  }
 
-console.log(state.test())
+  for(var o in obj){
+    this.count++;
+    internal(this).state[o] = obj[o];
+  }
 
-console.log(state.getState());
+  return obj;
+}
+
+Store.prototype.getState = function () {
+  console.log(internal(this).state); // Remove this later
+  return internal(this).state;
+}
+
+
+Store.prototype.saveStateHistory = function () {
+  let date = Date();
+  let state = Object.assign({...internal(this).state, date});
+  internal(this).hist.push(state);
+  return internal(this).hist;
+}
+
+Store.prototype.getStateHistory = function () {
+  console.log(internal(this).hist); // Remove this later
+  return internal(this).hist;
+}
+
+var store = new Store();
+store.setState({cool: 'stuff', prop: 'awesome'});
+
+setTimeout(() => {
+  store.setState({cool: 'better', wow: 'someting'});
+  store.getState();
+  store.getStateHistory();
+  store.saveStateHistory();
+  store.getStateHistory();
+}, 5000);
+
+store.saveStateHistory();
+store.getStateHistory();
+
+console.log(store.hist);
